e2b80f6b560ba077dbc65faacb698e37680f7ef3
ruslan-alybaev <ruslanalybaev7213@gmail.com>
2023-11-04 00:05:11 +0600

    first commit

---
+++ bin/lesson_6_homeworkk.dart
@@ -0,0 +1,111 @@
+import 'dart:io';
+// import 'dart:js_interop';
+
+import 'package:lesson_6_homeworkk/lesson_6_homeworkk.dart' as lesson_6_homeworkk;
+
+void main() {
+  // задача №1
+  // print("Введите порядковый номер пальца");
+  // String finger = stdin.readLineSync()!;
+  // if (finger == "1") {
+  //   print("большой палец");
+  // } else if (finger == "2") {
+  //   print("указательный палец");
+  // } else if (finger == "3") {
+  //   print("Средний палец");
+  // } else if (finger == "4") {
+  //   print("Безымянный палец");
+  // } else if (finger == "5") {
+  //   print("Мезинец палец");
+  // } else {
+  //   print("Error");
+  // }
+
+  // Задача №2
+  // print("Введите число от 0 до 59");
+  // var input = int.parse(stdin.readLineSync()!);
+  // if (input >= 0 && input <= 15) {
+  //   print("первая четверть");
+  // } else if (input > 15 && input <= 30) {
+  //   print("вторая четверть");
+  // } else if (input > 30 && input <= 45) {
+  //   print("третья четверть");
+  // } else if (input > 45 && input <= 59) {
+  //   print("четвертая четверть");
+  // } else {
+  //   print("Error");
+  // }
+
+  // Задача №3
+  // print("Введите на каком языке вывести дни недели: ru или eng");
+  // String lang = stdin.readLineSync()!;
+  // if (lang == "ru") {
+  //   List arr = ["Понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+  //   print(arr);
+  // } else if (lang == "en") {
+  //   List arr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  //   print(arr);
+  // } else {
+  //   print("Неподдерживаемый язык");
+  // }
+
+  // Задача №4
+  // String word = "abcde";
+  // if (word[0] == "a") {
+  //   print("да");
+  // } else {
+  //   print("нет");
+  // }
+  // print(word[0] == "a" ? "да" : "нет");
+
+  // Задача №5
+  // print("Введите значение от 1 до 4");
+  // var num = int.parse(stdin.readLineSync()!);
+  // if (num == 1) {
+  //   String result = "Зима";
+  //   print(result);
+  // } else if (num == 2) {
+  //   String result = "Весна";
+  //   print(result);
+  // } else if (num == 3) {
+  //   String result = "Лето";
+  //   print(result);
+  // } else if (num == 4) {
+  //   String result = "Осень";
+  //   print(result);
+  // } else {
+  //   print("Error");
+  // }
+
+  // Задача №6
+  // print("Введите число");
+  // var a = int.parse(stdin.readLineSync()!);
+  // print(a < 0 ? "Верно" /*: a == 0 ? "равно нулю"*/ : "Неверно"); // закоментированное проверял работу строчного условия если равно нулю
+
+  // Задача №7
+  // String num = "123123";
+  // if (num.length == 6) {
+  //   int firstSum = int.parse(num[0]) + int.parse(num[1]) + int.parse(num[2]);
+  //   int secondSum = int.parse(num[3]) + int.parse(num[4]) + int.parse(num[5]);
+
+  //   if (firstSum == secondSum) {
+  //     print("Да");
+  //   } else {
+  //     print("Нет");
+  //   }
+  // } else {
+  //   print("Введите строку из 6 цифр");
+  // }
+
+  // Задача №8
+//   String signal = "красный";
+//   if (signal == "красный") {
+//     print("Стоять");
+//   } else if (signal == "жёлтый") {
+//     print("Приготовиться");
+//   } else if (signal == "зелёный") {
+//     print("Можно идти");
+//   } else {
+//     print("Введите правильный цвет светофора");
+//   }
+}
---
+++ test/lesson_6_homeworkk_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_6_homeworkk/lesson_6_homeworkk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
